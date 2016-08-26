3b4ec8e82ba192b51791dfdaf5b6b0b89777605c
matthewmborda <matthewmborda@gmail.com>
2016-08-26 00:48:08 -0400

    end of day commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,21 +26,13 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
+
       .when('/movielookup/:movielookupID', {
         templateUrl: 'views/movielookup.html',
         controller: 'MovielookupCtrl',
         controllerAs: 'movielookup'
       })
-       .when('/sunburst/:movieID', {
-        templateUrl: 'views/sunburst.html',
-        controller: 'SunburstCtrl',
-        controllerAs: 'sunburst'
-      })
+ 
       .otherwise({
         redirectTo: '/'
       });
